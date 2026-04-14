5fad166f1692d69acc1563a70375d6840d70761f
nabelis-jasi <nabelisj65@gmail.com>
2026-04-14 11:12:41 +0200

    Delete src directory

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./style/Dashboard.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
-);
