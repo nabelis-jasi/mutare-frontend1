a50cac874110acca72d9d079a88f8335c0e52984
nabelis-jasi <nabelisj65@gmail.com>
2026-04-15 11:53:41 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from 'axios';
 
-const API_BASE_URL = process.env.VITE_API_URL || 'http://localhost:10000';
+const API_BASE_URL = process.env.VITE_API_URL || 'https://mutare-backend-tepj.onrender.com';
 
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api`,
