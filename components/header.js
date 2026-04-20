61ffa5e485898e214c8a0c9c24578321456f7723
nabelis-jasi <nabelisj65@gmail.com>
2026-04-20 15:09:11 +0200

    Update header.js

--- components/header.js
+++ components/header.js
@@ -5,7 +5,7 @@ export default {
         return `
             <div class="panel-header">
                 <div class="logo-container">
-                    <img src="https://logodix.com/logos/2094727" 
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Mutare_City_logo_2023.png" 
                          alt="City of Mutare Logo" 
                          class="city-logo"
                          onerror="this.onerror=null; this.src='https://placehold.co/60x60/228B22/white?text=MUTARE'">
